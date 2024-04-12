28fbc59bbe6b7667ecb820c5292e3dbce57fb0b6
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-04-12 14:31:42 +0400

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,9 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,49 +15,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
-
 /*Function prortypes*/
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
