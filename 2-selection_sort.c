d691e74936b6ccc7ba600e8639296e3a804a9aa7
root <nihad.nemetli@gmail.com>
2024-04-17 09:08:16 +0000

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * swap - change numbers place
+ * @change1: number1
+ * @change2: number2
+ * Return: void
+ */
+void swap(int *change1, int change2)
+{
+	int temp = *change1;
+	*change1 = &change2;
+	*change2 = temp;
+}
+
+/**
+ * selection_sort - selection sort
+ * @array: array
+ * @size: size
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	int i , j, min_index;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (arr[j] < arr[min_index])
+				min_index = j;
+		}
+
+		if (min_index != i)
+			swap(&array[min_index], &arr[i]);
+	}
+}
